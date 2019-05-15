4f00f06a3a28bf49cfe727003c37c9763fd94e4d
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-15 15:00:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string == whisper(string))
   return "I can't hear you!";
-  else if (string==shout.toUpperCase()) 
+  else if (string==shout()) 
   return "YES INDEED!";
   else if (string ==  "I love you, Grandma.")
   return "I love you, too.";
